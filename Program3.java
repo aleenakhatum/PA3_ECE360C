15ec781abb975778e220926e8e83c6ce6767ce96
aleenakhatum <aleenak@gmail.com>
2025-11-29 16:37:56 -0600

    initial begin

---
+++ Program3.java
@@ -0,0 +1,10 @@
+public class Program3 {
+
+    public int maxCoinValue(int[] cities) {
+        // Implement your dynamic programming algorithm here
+        // You may use helper functions as needed
+
+
+        return 0;
+    }
+}
