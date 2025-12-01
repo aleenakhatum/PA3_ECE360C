c77fdbb88a678cd56c681f9aaa9a51c6e806dfd9
aleenakhatum <aleenak@gmail.com>
2025-12-01 12:51:49 -0600

    working run config and working computePFS function

--- Program3.java
+++ Program3.java
@@ -1,10 +1,17 @@
 public class Program3 {
 
+    public void computePFS(int[] cities, int[] PFS) {
+        PFS[0] = cities[0];
+        for (int i = 1; i < PFS.length; i++) {
+            PFS[i] = PFS[i - 1] + cities[i];
+        }
+    }
+
     public int maxCoinValue(int[] cities) {
         // Implement your dynamic programming algorithm here
         // You may use helper functions as needed
-
-
+        int[] PFS = new int[cities.length];   // creates an array of length of input cities
+        computePFS(cities, PFS);
         return 0;
     }
 }
